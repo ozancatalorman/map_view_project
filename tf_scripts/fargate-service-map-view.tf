f3485236a06314c02675ae69d19ebd55246ada39
Ozan Catalorman <94858358+ozancatalorman@users.noreply.github.com>
2024-07-12 19:10:07 +0300

    terraform fixes

--- tf_scripts/fargate-service-map-view.tf
+++ tf_scripts/fargate-service-map-view.tf
@@ -1,31 +1,23 @@
-
-
-# locals.tf
 locals {
   service_name_map_view = "map-view"
-}
-
-# main.tf
-provider "aws" {
-  region = var.aws_region
 }
 
 module "map_view_fargate_service" {
   source    = "../modules/aws-ecs/fargate-service"
   providers = {
-    aws = aws
+    aws = aws.use2
   }
 
   project       = var.project
   environment   = var.environment
-  vpc_id        = var.vpc_id
+  vpc_id        = module.vpc.vpc_id
   cluster_id    = aws_ecs_cluster.this.id
 
-  service_name      = local.service_name_data_agent
+  service_name      = local.service_name_map_view
   service_port      = "8050"
   health_check_path = "/health"
 
-  host_header       = "dev.map.view"
+  host_header       = "dev.map-view.reefos.ai"
 
   desired_count          = "1"
   task_definition_cpu    = "1024"
@@ -40,67 +32,28 @@ module "map_view_fargate_service" {
     [
       {
         "name":  "REEF_MAP_VIEW",
-        "valueFrom": "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.project}/${local.service_name_map_view}/${var.environment}/reef_map_view"
+        "valueFrom": "arn:aws:ssm:${var.default_region}:${var.aws_account_id}:parameter/${var.project}/${local.service_name_map_view}/${var.environment}/reef_map_view"
       },
       {
         "name":  "REEF_MAP_VIEW_SKEY",
-        "valueFrom": "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.project}/${local.service_name_map_view}/${var.environment}/reef_map_view_skey"
+        "valueFrom": "arn:aws:ssm:${var.default_region}:${var.aws_account_id}:parameter/${var.project}/${local.service_name_map_view}/${var.environment}/reef_map_view_skey"
       },
       {
         "name":  "MONDAY_API_KEY",
-        "valueFrom": "arn:aws:ssm:${var.aws_region}:${var.aws_account_id}:parameter/${var.project}/${local.service_name_map_view}/${var.environment}/monday_api_key"
+        "valueFrom": "arn:aws:ssm:${var.default_region}:${var.aws_account_id}:parameter/${var.project}/${local.service_name_map_view}/${var.environment}/monday_api_key"
       }
     ]
   SECRETS
 
   ecr_repo = "${var.ecr_repo_base}/${local.service_name_map_view}"
 
-  private_subnet_ids    = var.private_subnet_ids
-  alb_security_group_id = var.alb_security_group_id
-  alb_listener_arn      = var.alb_listener_arn
+  private_subnet_ids    = module.vpc.private_subnets
+  alb_security_group_id = module.sg_alb.this_security_group_id
+  alb_listener_arn      = module.alb.this_lb_default_listener
   tg_priority           = 104
 
   execution_role_arn = module.service_execution_role.this_iam_role_arn
   task_role_arn      = module.service_task_role.this_iam_role_arn
 
   log_retention_in_days = "14"
-}
-
-resource "aws_ssm_parameter" "reef_map_view" {
-  name  = "/${var.project}/${local.service_name_map_view}/${var.environment}/reef_map_view"
-  type  = "SecureString"
-  value = "map_view_app_password"
-}
-
-resource "aws_ssm_parameter" "reef_map_view_skey" {
-  name  = "/${var.project}/${local.service_name_map_view}/${var.environment}/reef_map_view_skey"
-  type  = "SecureString"
-  value = "map_view_app_secret_key"
-}
-
-resource "aws_ssm_parameter" "monday_api_key" {
-  name  = "/${var.project}/${local.service_name_map_view}/${var.environment}/monday_api_key"
-  type  = "SecureString"
-  value = "your_api_key"
-}
-
-resource "aws_ecs_cluster" "this" {
-  name = "${var.project}-${var.environment}-ecs-cluster"
-}
-
-module "service_execution_role" {
-  source = "../modules/aws-iam-role"
-  role_name = "${var.project}-${var.environment}-ecs-execution-role"
-  policies = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  ]
-}
-
-module "service_task_role" {
-  source = "../modules/aws-iam-role"
-  role_name = "${var.project}-${var.environment}-ecs-task-role"
-  policies = [
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-  ]
 }
